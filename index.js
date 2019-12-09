a62059915512a8c66a0e28dd6b9be00fa5bec634
Enoka Jaona <ejaona@gmail.com>
2019-12-09 16:49:31 -0600

    last change

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ server.use(helmet())
 server.use(express.json());
 server.use(cors());
 server.use( (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://simespepper.netlify.com/checkout"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "https://simespepper.com/checkout"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
